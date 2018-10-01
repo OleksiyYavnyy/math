8403b03bc5e1f3286fd4eb14f62a582fcbde5a5b
Oleksiy <oleksiyyavnyy@gmail.com>
2018-10-01 13:59:54 +0000

    individual

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function solveQuadrEq(a, b, c) {
     const d = calcDiscr(a, b, c);
     if (d > 0) {
-        x1 = ((-b + Math.sqrt(d))/(2*a));
-        x2 = ((-b - Math.sqrt(d))/(2*a));
+       const x1 = ((-b + Math.sqrt(d))/(2*a));
+       const x2 = ((-b - Math.sqrt(d))/(2*a));
         return 'x1 = ' + x1 + ' x2 = ' + x2;
     } else if (d === 0) {
-        x = -b / (2 * a);
+      const  x = -b / (2 * a);
         return 'x = ' + x;
     } else {
         return 'Sorry Michael just math';
